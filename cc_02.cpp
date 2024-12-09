931ba52e30596d1f2d0d557954eba08d2dabe618
AmadeuEFA <amadeuaraujo@gmail.com>
2024-12-09 14:15:07 +0000

    Create cc_02.cpp
    Exercício 02

---
+++ cc_02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main( ) {
+    int age, float altura;
+    count << "enter your age: ", enter your altura;
+    cin >> age, altura; 
+    count << "your age is", "your altura is";
+    return 0;
+}
+ 
