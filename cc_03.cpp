1fe5251d630c9246f31dfd15570fa9df58dbc285
AmadeuEFA <amadeuaraujo@gmail.com>
2024-12-09 15:59:27 +0000

    Update cc_03.cpp
    correção de erros

--- cc_03.cpp
+++ cc_03.cpp
@@ -1,13 +1,4 @@
 #include <iostream>
-using namespace std;
-int main( ) {
-    int age, float altura;
-    count << "enter your age: ", enter your altura;
-    cin >> age, altura; 
-    count << "your age is", "your altura is";
-    return 0;
-}
- #include <iostream>
 
 using namespace std;
 
